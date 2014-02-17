f73e9b2d482101de0be4bd9d75ef3b90b0b3faba
alyona- <alyona_om@mail.ru>
2014-02-18 00:36:32 +0200

    Change all

--- script.js
+++ script.js
@@ -2,24 +2,20 @@ var bar = {};
 bar.name = document.querySelector('.bar_color');
 bar.val =document.querySelector('.bar_val');
 bar.procent =20;
+bar.progress = bar.procent;
 bar.name.style.width ='20%';
-bar.width =function(width){
+bar.width = function(width){
     if(width>=0 && width<=100) {
-        if(bar.procent<width){
-            var i= setInterval(function(){
-                bar.val.innerText = bar.procent+'%';
-                bar.name.style.width=bar.procent+'%';
-                bar.procent++;
-
-                if(bar.procent>width) clearInterval(i);
-            },50);}
-        else if(bar.procent>width){
-            var i=setInterval(function(){
-                bar.val.innerText=bar.procent+'%';
-                bar.name.style.width=bar.procent+'%';
-                bar.procent--;
-                if(bar.procent<width) clearInterval(i);
-            },50); }
-
-    }}
-bar.val.innerHTML = bar.procent+'%';
+        bar.name.style.width = width+'%';
+        var i = setInterval(function(){
+            bar.val.innerText = bar.progress+"%";
+            if(bar.progress<width) bar.progress++;
+            else if(bar.progress>width)bar.progress--;
+            if(bar.progress==width) {
+                bar.val.innerText = width+'%';
+                clearInterval(i);
+            }
+        },0.8);
+    }
+}
+bar.val.innerHTML = bar.progress+'%';
